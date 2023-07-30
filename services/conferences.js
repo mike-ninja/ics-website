db53bee49ae3c785f2ec7bd69c59fb9e318ae626
mike-ninja <mikeedubas@hotmail.com>
2023-07-30 16:52:02 +0300

    first commit

---
+++ services/conferences.js
@@ -0,0 +1,96 @@
+import { Client } from "@notionhq/client";
+import { NotionToMarkdown } from "notion-to-md";
+
+const notion = new Client({ auth: process.env.NOTION_ACCESS_TOKEN });
+const n2m = new NotionToMarkdown({ notionClient: notion });
+
+const pageToPostTransformer = (page) => {
+  let cover = page.cover;
+  console.log(cover);
+  switch (cover.type) {
+    case "file":
+      cover = page.cover.file;
+      break;
+    case "external":
+      cover = page.cover.external.url;
+      break;
+    default:
+      cover = "";
+  }
+
+  return {
+    id: page.id,
+    cover: cover,
+    title: page.properties.Name.title[0].plain_text,
+    tags: page.properties.Tags.multi_select,
+    description: page.properties.Description.rich_text[0].plain_text,
+    date: page.properties.Updated.last_edited_time,
+    slug: page.properties.Slug.formula.string,
+  };
+};
+
+const getConferences = async () => {
+  const database = process.env.NOTION_BLOG_DATABASE_ID ?? "";
+  const response = await this.client.databases.query({
+    database_id: database,
+    filter: {
+      property: "Published",
+      checkbox: {
+        equals: true,
+      },
+    },
+    sorts: [
+      {
+        property: "Updated",
+        direction: "descending",
+      },
+    ],
+  });
+
+  return response.results.map((res) => {
+    return pageToPostTransformer(res);
+  });
+};
+
+const getConference = async (slug) => {
+  let post, markdown;
+
+  const database = process.env.NOTION_BLOG_DATABASE_ID ?? "";
+  // list of blog posts
+  const response = await this.client.databases.query({
+    database_id: database,
+    filter: {
+      property: "Slug",
+      formula: {
+        text: {
+          equals: slug, // slug
+        },
+      },
+      // add option for tags in the future
+    },
+    sorts: [
+      {
+        property: "Updated",
+        direction: "descending",
+      },
+    ],
+  });
+
+  if (!response.results[0]) {
+    throw "No results available";
+  }
+
+  // grab page from notion
+  const page = response.results[0];
+
+  const mdBlocks = await n2m.pageToMarkdown(page.id);
+  markdown = n2m.toMarkdownString(mdBlocks);
+  post = pageToPostTransformer(page);
+
+  return {
+    post,
+    markdown,
+  };
+};
+
+export default { getConference, getConferences };
