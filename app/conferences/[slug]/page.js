2a74c94ded98e0baeb43149455026f6644f9659c
mike-ninja <mikeedubas@hotmail.com>
2023-08-01 09:49:46 +0300

    update

--- app/conferences/[slug]/page.js
+++ app/conferences/[slug]/page.js
@@ -1,5 +1,6 @@
 import conferencesService from "@/services/conferences";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 const page = async ({ params }) => {
   const conference = await conferencesService.getConference(params.slug);
@@ -7,9 +8,7 @@ const page = async ({ params }) => {
   return (
     <>
       <div className="container markdown">
-        <ReactMarkdown>
-          {conference.markdown.parent}
-        </ReactMarkdown>
+        <ReactMarkdown children={conference.markdown.parent} remarkPlugins={[remarkGfm]} />
       </div>
     </>
   );
