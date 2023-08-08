f146abc46a3854820fc9323bc5bcb9a66910ab8d
mike-ninja <mikeedubas@hotmail.com>
2023-08-08 10:42:25 +0300

    adjustments on conference cards

--- components/home/Conference.jsx
+++ components/home/Conference.jsx
@@ -1,23 +1,50 @@
 "use client";
 
 import Link from "next/link";
+import { BiSolidRightArrow } from "react-icons/bi";
+import { BiSolidDownArrow } from "react-icons/bi";
 import { useState } from "react";
 import dayjs from "dayjs";
 
 const localizedFormat = require("dayjs/plugin/localizedFormat");
 dayjs.extend(localizedFormat);
 
+// <BiSolidRightArrow style={{display: showSpeakers ? "none" : ""}}/>
+// <BiSolidDownArrow style={{display: showSpeakers ? "" : "none"}}/>
 const Speakers = ({ speakers }) => {
   const [showSpeakers, setShowSpeakers] = useState(false);
   return (
-    <div>
-      <div>
-        <button onClick={() => setShowSpeakers((prev) => !prev)}>
-          Speakers
-        </button>
+    <div className="text-dark_brown">
+      <div
+        onClick={() => setShowSpeakers((prev) => !prev)}
+        className="inline-flex items-center"
+      >
+        <div className="mb-1 mr-1">
+          <BiSolidRightArrow
+            className="overflow-hidden transition-all ease-in duration-400"
+            style={{
+              height: showSpeakers ? "0" : "auto",
+              opacity: showSpeakers ? "0" : "1",
+            }}
+          />
+          <BiSolidDownArrow
+            className="overflow-hidden transition-all ease-in duration-400"
+            style={{
+              height: showSpeakers ? "auto" : "0",
+              opacity: showSpeakers ? "1" : "0",
+            }}
+          />
+        </div>
+        <div className="align-middle">Speakers</div>
       </div>
-      <div style={{ display: showSpeakers ? "" : "none" }}>
-        {speakers.map((speaker) => <h3>{speaker.title}</h3>)}
+      <div
+        className="overflow-hidden transition-all ease-linear duration-400"
+        style={{
+          height: showSpeakers ? "auto" : "0",
+          opacity: showSpeakers ? "1" : "0",
+        }}
+      >
+        {speakers.map((speaker) => <div className="capitalize">{speaker.title}</div>)}
       </div>
     </div>
   );
@@ -35,22 +62,22 @@ const Conference = ({ conference, speakers }) => {
   });
 
   return (
-    <Link
-      href={`/conferences/${conference.slug}`}
-    >
-      <div className="my-5 md:my-7 flex transition-all duration-300 hover:scale-105">
-        <div className="date_wrapper">
-          <h2 className="date_flipped">
-            {dayjs(conference.date.start).format("DD MMM")}
-          </h2>
-        </div>
-        <div className="w-full rounded-lg border-2 border-dark_brown text-right pr-2 pt-3">
-          <h3>{conference.title}</h3>
-          <h4>{conference.hotel}</h4>
-          <Speakers speakers={conferenceSpeakers} />
-        </div>
+    <div className="flex py-3 uppercase">
+      <div className="relative flex-none date_wrapper w-10 uppercase text-blood_red">
+        <span className="text-[1.7rem] font-mono font-semibold tracking-tighter absolute transform rotate-[270deg] top-1/2 translate-y-[-50%] translate-x-[-0.8rem]">
+          {dayjs(conference.date.start).format("DD")}
+          &nbsp;
+          {dayjs(conference.date.start).format("MMM")}
+        </span>
       </div>
-    </Link>
+      <div className="flex-1 flex-col text-right border-dark_brown border-2 rounded-lg py-2 pr-3">
+        <div className="text-light_brown font-semibold text-[1.2rem]">
+          {conference.title}
+        </div>
+        <div className="text-dark_brown">{conference.hotel}</div>
+        <Speakers speakers={conferenceSpeakers} />
+      </div>
+    </div>
   );
 };
 
