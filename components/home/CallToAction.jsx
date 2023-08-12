98dc65a4eeaa67a44fd1da4964c3e679dc486a02
mike-ninja <mikeedubas@hotmail.com>
2023-08-12 20:44:05 +0300

    update

--- components/home/CallToAction.jsx
+++ components/home/CallToAction.jsx
@@ -7,51 +7,57 @@ import { RiPresentationFill } from "react-icons/ri";
 const CallToAction = () => {
   return (
     <section className="bg-light_beige">
-      <div className="container text-center text-light_brown text-2xl">
+      <div className="container text-center text-light_blue">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
           <div>
-            <div className="flex flex-col items-center bg-light_beige py-10 rounded-xl transition-all hover:scale-[1.02]">
-              <HiSpeakerphone className="text-4xl" />
-              Become a Speaker
-              <small className="italic text-dark_brown">
+            <div className="flex flex-col items-center bg-off_white py-10 rounded-xl transition-all hover:scale-[1.02]">
+              <HiSpeakerphone className="text-4xl text-metal_gray mb-3" />
+              <strong className="text-2xl">
+                Become a Speaker
+              </strong>
+              <p className="italic text-metal_gray">
                 Share your passion through ICS
-              </small>
+              </p>
             </div>
             <a
               href=""
-              className="btn btn_variant bg-light_beige"
+              className="border-b-[1px] border-metal_gray inline-block mt-4 transition-all hover:scale-105 text-2xl"
             >
-              <span className="text-light_brown">Click Here</span>
+              <span className="text-metal_gray">Click Here</span>
             </a>
           </div>
           <div>
-            <div className="flex flex-col items-center bg-light_beige py-10 rounded-xl transition-all hover:scale-[1.02]">
-              <PiMedalFill className="text-4xl" />
-              Become a Sponsor
-              <small className="italic text-dark_brown">
+            <div className="flex flex-col items-center bg-off_white py-10 rounded-xl transition-all hover:scale-[1.02]">
+              <PiMedalFill className="text-4xl text-metal_gray mb-3" />
+              <strong className="text-2xl">
+                Become a Sponsor
+              </strong>
+              <p className="italic text-metal_gray">
                 Partner with us at ICS
-              </small>
+              </p>
             </div>
             <a
               href=""
-              className="btn btn_variant bg-light_beige"
+              className="border-b-[1px] border-metal_gray inline-block mt-4 transition-all hover:scale-105 text-2xl"
             >
-              <span className="text-light_brown">Click Here</span>
+              <span className="text-metal_gray">Click Here</span>
             </a>
           </div>
           <div>
-            <div className="flex flex-col items-center bg-light_beige py-10 rounded-xl transition-all hover:scale-[1.02]">
-              <RiPresentationFill className="text-4xl" />
-              Become a Exhibitor
-              <small className="italic text-dark_brown">
+            <div className="flex flex-col items-center bg-off_white py-10 rounded-xl transition-all hover:scale-[1.02]">
+              <RiPresentationFill className="text-4xl text-metal_gray mb-3" />
+              <strong className="text-2xl">
+                Become a Exhibitor
+              </strong>
+              <p className="italic text-metal_gray">
                 Connect, Engage, & Exhibit at ICS
-              </small>
+              </p>
             </div>
             <a
               href=""
-              className="btn btn_variant bg-light_beige"
+              className="border-b-[1px] border-metal_gray inline-block mt-4 transition-all hover:scale-105 text-2xl"
             >
-              <span className="text-light_brown">Click Here</span>
+              <span className="text-metal_gray">Click Here</span>
             </a>
           </div>
         </div>
