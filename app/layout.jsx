be8fdd7c93688eb82cade4b9e76f3659fe5c1ddb
mike-ninja <mikeedubas@hotmail.com>
2023-08-13 11:10:09 +0300

    updates

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html className="scroll-smooth" lang="en">
+    <html className="scroll-smooth scrollbar-hide" lang="en">
       <body>
         <Nav />
         <main>
