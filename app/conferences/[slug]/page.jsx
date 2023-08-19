d4c6f2bd91abe0fc1c9ab00cd7d18291cd6e88a4
mike-ninja <mikeedubas@hotmail.com>
2023-08-19 15:02:27 +0300

    small update to conference page

--- app/conferences/[slug]/page.jsx
+++ app/conferences/[slug]/page.jsx
@@ -1,12 +1,31 @@
+import CallToAction from "@/components/CallToAction";
 import conferencesService from "@/services/conferences";
 import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
+// import Link from "next/link";
+import dayjs from "dayjs";
+import Image from "next/image";
 
-const PageHeader = ({ cover }) => {
+const PageHeader = ({ post }) => {
+  const localizedFormat = require("dayjs/plugin/localizedFormat");
+  dayjs.extend(localizedFormat);
   return (
-    <div className="my-3">
-      <img src={cover} />
-    </div>
+    <section>
+      <div className="container">
+        <div className="grid grid-cols-1">
+          <Image src={post.cover} width={600} height={400} />
+          <h1>{post.title}</h1>
+          <span>
+            🗓️
+            {dayjs(post.date.start).format("DD")}
+            -
+            {dayjs(post.date.end).format("DD MMM YYYY")}
+          </span>
+          <a href={post.address} target="_blank" rel="noreferrer">
+            {post.hotel}
+          </a>
+        </div>
+      </div>
+    </section>
   );
 };
 
@@ -15,16 +34,18 @@ const page = async ({ params }) => {
     params.slug,
   );
 
-  console.log(markdown.parent)
+  console.log(post);
   return (
     <>
-      <div className="container markdown">
-        <PageHeader cover={post.cover} />
-        <ReactMarkdown
-          children={markdown.parent}
-          remarkPlugins={[remarkGfm]}
-        />
-      </div>
+      <PageHeader post={post} />
+      <section>
+        <div className="container markdown">
+          <ReactMarkdown
+            children={markdown.parent}
+          />
+        </div>
+      </section>
+      <CallToAction />
     </>
   );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Header from "@/components/home/Header";
 import Conferences from "@/components/home/Conferences";
 import HomeAbout from "@/components/home/HomeAbout";
 import Advantages from "@/components/home/Advantages";
-import CallToAction from "@/components/home/CallToAction";
+import CallToAction from "@/components/CallToAction";
 import FooterContact from "@/components/FooterContact";
 
 export default async function Home() {
